8007dbbc3659e143b2d4c6fd41c985ad645d4747
shan-guru <43489042+shan-guru@users.noreply.github.com>
2023-05-26 19:02:06 +0800

    Adding Beautiful Places on Earth.

---
+++ src/main/java/com/example/spring/demo/BeautifulWorldController.java
@@ -0,0 +1,22 @@
+package com.example.spring.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo-v2/api/v1")
+public class BeautifulWorldController {
+
+    @GetMapping("/places")
+    public Flux<String> beautifulPlaces() {
+        List<String> list = List.of("Zhangye Danxia Geopark, China",
+                "Venice, Italy",
+                "Banff National Park, Canada",
+                "Great Ocean Road, Australia");
+        return Flux.fromIterable(list);
+    }
+}
