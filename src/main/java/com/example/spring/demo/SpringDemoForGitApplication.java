cebc22b5fa95df0744641300d544a73937011f4e
shan-guru <43489042+shan-guru@users.noreply.github.com>
2023-05-26 18:37:33 +0800

    Draft version of commit.

---
+++ src/main/java/com/example/spring/demo/SpringDemoForGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoForGitApplication.class, args);
+	}
+
+}
